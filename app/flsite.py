b1014a6d81f131ca6fa4b24738b4a3dda0ef45c7
Dm1triiSmirnov <smirnov0719@gmail.com>
2022-08-10 14:33:28 +0300

    add temlates

--- app/flsite.py
+++ app/flsite.py
@@ -3,14 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+menu = ['Установка',
+        'Первое приложение',
+        'Обратная связь']
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           menu=menu)
 
 
 @app.route('/about')
 def aboute():
-    return 'about'
+    return render_template('about.html',
+                           title='О сайте',
+                           menu=menu)
 
 
 if __name__ == '__main__':
