002afedefa94c8f2614656a5c2f10f3103f840c8
Dm1triiSmirnov <smirnov0719@gmail.com>
2022-08-12 18:02:28 +0300

    add styles.css, contact.html, add route for conact

--- app/flsite.py
+++ app/flsite.py
@@ -1,23 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 
-menu = ['Установка',
-        'Первое приложение',
-        'Обратная связь']
+menu = [{'name': 'Установка', 'url': 'install-flask'},
+        {'name': 'Первое приложение', 'url': 'first-app'},
+        {'name': 'Обратная связь', 'url': 'contact'}]
 
 @app.route('/')
 def index():
+    print(url_for('index'))
     return render_template('index.html',
                            menu=menu)
 
 
+@app.route('/profile/<username>')
+def profile(username):
+    return f'Пользователь: {username}'
+
+
 @app.route('/about')
-def aboute():
+def about():
+    print(url_for('about'))
     return render_template('about.html',
                            title='О сайте',
                            menu=menu)
+
+
+@app.route('/contact', methods=['POST', 'GET'])
+def contact():
+    if request.method == 'POST':
+        print(request.form)
+
+    return render_template('contact.html', title='Обратная связь', menu=menu)
+
 
 
 if __name__ == '__main__':
