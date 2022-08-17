8f2fc1a537d0a0f3b7b09bdb370ae757e18dbc8e
Dm1triiSmirnov <smirnov0719@gmail.com>
2022-08-17 16:54:05 +0300

    add login form, templates, connect db

--- app/flsite.py
+++ app/flsite.py
@@ -1,31 +1,60 @@
-from flask import Flask, render_template, url_for, request, flash
+import sqlite3
+import os
+from flask import (Flask, render_template, url_for,
+                    request, flash, session,
+                   redirect, abort, g)
+
+
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = 'kal4jHCvj6#6sfs6df%6sdfssDf78Jjc'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'kal4jhcvj66sfs6df6sdfssdf78jjc'
-
+app.config.from_object(__name__)
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
 
 menu = [{'name': 'Установка', 'url': 'install-flask'},
         {'name': 'Первое приложение', 'url': 'first-app'},
         {'name': 'Обратная связь', 'url': 'contact'}]
 
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
 @app.route('/')
 def index():
-    print(url_for('index'))
+    db = get_db()
     return render_template('index.html',
                            menu=menu)
-
-
-@app.route('/profile/<username>')
-def profile(username):
-    return f'Пользователь: {username}'
 
 
 @app.route('/about')
 def about():
     print(url_for('about'))
-    return render_template('about.html',
-                           title='О сайте',
-                           menu=menu)
+    return render_template('about.html', title='О сайте', menu=menu)
 
 
 @app.route('/contact', methods=['POST', 'GET'])
@@ -39,6 +68,28 @@ def contact():
     return render_template('contact.html', title='Обратная связь', menu=menu)
 
 
+@app.route('/profile/<username>')
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+
+    return f'Профиль пользователя: {username}'
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == 'admin' and request.form['psw'] == '123':
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title='Авторизация', menu=menu)
+
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('page404.html', title='Страница не найдена', menu=menu), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
