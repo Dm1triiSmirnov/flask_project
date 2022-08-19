8d7259b419cc20a4df9619c8f24bbc71d53cad46
Dm1triiSmirnov <smirnov0719@gmail.com>
2022-08-19 16:17:17 +0300

    add function to add posts, fix templates

--- app/flsite.py
+++ app/flsite.py
@@ -4,6 +4,8 @@ from flask import (Flask, render_template, url_for,
                     request, flash, session,
                    redirect, abort, g)
 
+from app.FDataBase import FDataBase
+
 
 DATABASE = '/tmp/flsite.db'
 DEBUG = True
@@ -13,9 +15,9 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
 
-menu = [{'name': 'Установка', 'url': 'install-flask'},
-        {'name': 'Первое приложение', 'url': 'first-app'},
-        {'name': 'Обратная связь', 'url': 'contact'}]
+# menu = [{'name': 'Установка', 'url': 'install-flask'},
+#         {'name': 'Первое приложение', 'url': 'first-app'},
+#         {'name': 'Обратная связь', 'url': 'contact'}]
 
 
 def connect_db():
@@ -44,52 +46,88 @@ def close_db(error):
         g.link_db.close()
 
 
+dbase = None
+@app.before_request
+def before_request():
+    """Установление соединения с БД перед выполнением запроса"""
+    global dbase
+    db = get_db()
+    dbase = FDataBase(db)
+
+
 @app.route('/')
 def index():
-    db = get_db()
     return render_template('index.html',
-                           menu=menu)
+                           menu=dbase.getMenu(),
+                           posts=dbase.getPostsAnonce())
 
 
-@app.route('/about')
-def about():
-    print(url_for('about'))
-    return render_template('about.html', title='О сайте', menu=menu)
-
-
-@app.route('/contact', methods=['POST', 'GET'])
-def contact():
-    if request.method == 'POST':
-        if len(request.form['username']) > 2:
-            flash('Сообщение отправлено', category='success')
+@app.route("/add_post", methods=["POST", "GET"])
+def addPost():
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
         else:
-            flash('Ошибка отправки', category='error')
+            flash('Ошибка добавления статьи', category='error')
 
-    return render_template('contact.html', title='Обратная связь', menu=menu)
-
-
-@app.route('/profile/<username>')
-def profile(username):
-    if 'userLogged' not in session or session['userLogged'] != username:
-        abort(401)
-
-    return f'Профиль пользователя: {username}'
+    return render_template('add_post.html', menu=dbase.getMenu(), title="Добавление статьи")
 
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if 'userLogged' in session:
-        return redirect(url_for('profile', username=session['userLogged']))
-    elif request.method == 'POST' and request.form['username'] == 'admin' and request.form['psw'] == '123':
-        session['userLogged'] = request.form['username']
-        return redirect(url_for('profile', username=session['userLogged']))
+@app.route('/post/<int:id_post>')
+def showPost(id_post):
+    title, post = dbase.getPost(id_post)
+    if not title:
+        abort(404)
 
-    return render_template('login.html', title='Авторизация', menu=menu)
+    return render_template('post.html',
+                           menu=dbase.getMenu(),
+                           title=title,
+                           post=post)
 
-
-@app.errorhandler(404)
-def pageNotFound(error):
-    return render_template('page404.html', title='Страница не найдена', menu=menu), 404
+#
+# @app.route('/about')
+# def about():
+#     print(url_for('about'))
+#     return render_template('about.html', title='О сайте', menu=dbase.getMenu())
+#
+#
+# @app.route('/contact', methods=['POST', 'GET'])
+# def contact():
+#     if request.method == 'POST':
+#         if len(request.form['username']) > 2:
+#             flash('Сообщение отправлено', category='success')
+#         else:
+#             flash('Ошибка отправки', category='error')
+#
+#     return render_template('contact.html', title='Обратная связь', menu=dbase.getMenu())
+#
+#
+# @app.route('/profile/<username>')
+# def profile(username):
+#     if 'userLogged' not in session or session['userLogged'] != username:
+#         abort(401)
+#
+#     return f'Профиль пользователя: {username}'
+#
+#
+# @app.route('/login', methods=['POST', 'GET'])
+# def login():
+#     if 'userLogged' in session:
+#         return redirect(url_for('profile', username=session['userLogged']))
+#     elif request.method == 'POST' and request.form['username'] == 'admin' and request.form['psw'] == '123':
+#         session['userLogged'] = request.form['username']
+#         return redirect(url_for('profile', username=session['userLogged']))
+#
+#     return render_template('login.html', title='Авторизация', menu=dbase.getMenu())
+#
+#
+# @app.errorhandler(404)
+# def pageNotFound(error):
+#     return render_template('page404.html', title='Страница не найдена', menu=dbase.getMenu()), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
