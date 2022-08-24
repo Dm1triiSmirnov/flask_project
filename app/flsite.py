2be241d6074d671360791b545d69e9ba34142b5b
Dm1triiSmirnov <smirnov0719@gmail.com>
2022-08-24 17:13:29 +0300

    fix add_post, refactoring

--- app/flsite.py
+++ app/flsite.py
@@ -66,7 +66,7 @@ def index():
 def addPost():
     if request.method == "POST":
         if len(request.form['name']) > 4 and len(request.form['post']) > 10:
-            res = dbase.addPost(request.form['name'], request.form['post'])
+            res = dbase.addPost(request.form['name'], request.form['post'], request.form['url'])
             if not res:
                 flash('Ошибка добавления статьи', category='error')
             else:
@@ -77,9 +77,9 @@ def addPost():
     return render_template('add_post.html', menu=dbase.getMenu(), title="Добавление статьи")
 
 
-@app.route('/post/<int:id_post>')
-def showPost(id_post):
-    title, post = dbase.getPost(id_post)
+@app.route('/post/<alias>')
+def showPost(alias):
+    title, post = dbase.getPost(alias)
     if not title:
         abort(404)
 
@@ -88,46 +88,6 @@ def showPost(id_post):
                            title=title,
                            post=post)
 
-#
-# @app.route('/about')
-# def about():
-#     print(url_for('about'))
-#     return render_template('about.html', title='О сайте', menu=dbase.getMenu())
-#
-#
-# @app.route('/contact', methods=['POST', 'GET'])
-# def contact():
-#     if request.method == 'POST':
-#         if len(request.form['username']) > 2:
-#             flash('Сообщение отправлено', category='success')
-#         else:
-#             flash('Ошибка отправки', category='error')
-#
-#     return render_template('contact.html', title='Обратная связь', menu=dbase.getMenu())
-#
-#
-# @app.route('/profile/<username>')
-# def profile(username):
-#     if 'userLogged' not in session or session['userLogged'] != username:
-#         abort(401)
-#
-#     return f'Профиль пользователя: {username}'
-#
-#
-# @app.route('/login', methods=['POST', 'GET'])
-# def login():
-#     if 'userLogged' in session:
-#         return redirect(url_for('profile', username=session['userLogged']))
-#     elif request.method == 'POST' and request.form['username'] == 'admin' and request.form['psw'] == '123':
-#         session['userLogged'] = request.form['username']
-#         return redirect(url_for('profile', username=session['userLogged']))
-#
-#     return render_template('login.html', title='Авторизация', menu=dbase.getMenu())
-#
-#
-# @app.errorhandler(404)
-# def pageNotFound(error):
-#     return render_template('page404.html', title='Страница не найдена', menu=dbase.getMenu()), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
